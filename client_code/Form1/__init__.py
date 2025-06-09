e1619790661ece1eb566d0c287b6b4e4482641bf
andreas.noviananto26@gmail.com <andreas.noviananto26@gmail.com>
2025-06-09 12:49:06 +0000

    Created Form1 and c7e84d68cd4b3432a294e9b775328ee64bcbdfc4_s2_n2.png; Edited settings

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+import anvil.server
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    pass
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call("test",self.file_loader_1.file)
+    text=anvil.server.call("hasil")
+    self.label_4.text=text
+    pass
